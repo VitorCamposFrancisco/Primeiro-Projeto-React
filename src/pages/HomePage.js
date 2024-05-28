6434d178489940724c01871447c0532e5805ba7e
Your Name <you@example.com>
2024-05-27 22:07:40 -0300

    Boa

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,6 +19,15 @@ const HomePage = () => {
         filmesLivros();
     });
 
+    const reduceCards = (acc, cur, index) => {
+        const groupIndex = Math.floor(index / 4);
+        if (!acc[groupIndex]) acc[groupIndex] = [];
+        acc[groupIndex].push(cur);
+        console.log(acc);
+        return acc;
+    };
+
+
     return (
         <div >
             <Container className="mt-5 mb-5 carousel">
@@ -37,126 +46,51 @@ const HomePage = () => {
             <Container className="mt-5 position-relative shadow-lg p-1 fundo rounded">
                 <h2 class="d-flex justify-content-center text-light fw-light">FILMES</h2>
             </Container>
-            <Carousel s className="ms-3 mt-5 carrosel">
-                {filmes.map((filme, index) => (
+            <Container>
+                <Carousel className="shadow-lg  bg-body-light mb-5 rounded">
+                    {filmes.reduce(reduceCards, []).map((item, index) => (
+                        <Carousel.Item key={index}>
+                            {item.map((filme, index) => (
+                                <Card style={{ width: '17rem' }}>
+                                    <img src={filme.imagens[2]} alt={filme.titulo} className="col-12"></img>
+                                    <Carousel.Caption>
+                                        <h3>{filme.title}</h3>
+                                        <p>{filme.sinopse}</p>
+                                    </Carousel.Caption>
+                                </Card>
+                            ))}
+
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <Container className="mt-5 position-relative shadow-lg p-1 fundo rounded">
+                    <h2 class="d-flex justify-content-center text-light fw-light">LIVROS</h2>
+                </Container>
+                <Carousel className="ms-3 mt-5 carrosel">
                     <Carousel.Item>
                         <Row>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <div className="verFilmes"></div>
-                                            <Button className="verFilmes" variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
+                            {filmes.map((filme, index) => (
+                                <Col sm>
+                                    <div >
+                                        <Card style={{ width: '17rem' }}>
+                                            <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
+                                            <Card.Body>
+                                                <Card.Title>{filme.title}</Card.Title>
+                                                <Card.Text>
+                                                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                                                </Card.Text>
+                                                <div className="verFilmes">
+                                                    <Button className="verFilmes" variant="primary">Ver filme</Button></div>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                </Col>
+                            ))}
                         </Row>
                     </Carousel.Item>
-                ))}
 
-            </Carousel>
-            <Container className="mt-5 position-relative shadow-lg p-1 fundo rounded">
-                <h2 class="d-flex justify-content-center text-light fw-light">LIVROS</h2>
+                </Carousel>
             </Container>
-            <Carousel s className="ms-3 mt-5 carrosel">
-                {filmes.map((filme, index) => (
-                    <Carousel.Item>
-                        <Row>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.imagens[2]} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.url_thumbnail} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                            <Col sm>
-                                <div >
-                                    <Card style={{ width: '17rem' }}>
-                                        <Card.Img variant="top" src={filme.imagens[2]} alt={filme.titulo} className="col-12" />
-                                        <Card.Body>
-                                            <Card.Title>{filme.title}</Card.Title>
-                                            <Card.Text>
-                                                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                            </Card.Text>
-                                            <Button variant="primary">Ver filme</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Carousel.Item>
-                ))}
-
-            </Carousel>
         </div>
     )
 }
